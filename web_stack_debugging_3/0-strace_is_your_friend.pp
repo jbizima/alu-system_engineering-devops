3437d7504edc195100c9835a5a3133d8826e46c5
jbizima <j.bizima@alustudent.com>
2022-12-16 17:55:14 +0000

    tasks

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,6 @@
-# Web Stack debugging increase limit of open files per user
-exec { 'fix--for-nginx':
-  environment => ['DIR=/etc/default/nginx',
-                  'OLD=ULIMIT="-n 15"',
-                  'NEW=ULIMIT="-n 15000"'],
-  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR; sudo service nginx restart',
-  path        => ['/usr/bin', '/bin'],
-  returns     => [0, 1]
+# fixes Apache 500 error by fixing typo in wordpress
+exec { 'fix typo':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
